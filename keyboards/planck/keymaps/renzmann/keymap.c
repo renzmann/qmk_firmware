5ae1870745d32e122d5cc111c01f58dd3032cb6f
Robert Enzmann <robert.enzmann@antuit.com>
2020-06-06 20:37:35 -0500

    add media controls, fix right arrow, and move f keys

--- keyboards/planck/keymaps/renzmann/keymap.c
+++ keyboards/planck/keymaps/renzmann/keymap.c
@@ -109,7 +109,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_planck_grid(
     _______, _______, KC_TAB , KC_BSPC, KC_BSPC, _______, _______, KC_PGDN, KC_UP  , KC_PGUP, KC_INS,  _______,
-    _______, KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_UNDS, KC_EQL,
+    _______, KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
     KC_CAPS, _______, _______, KC_DEL , KC_DEL , _______, _______, KC_HOME, KC_END , _______, _______, _______,
     _______, _______, _______, _______, _______, KC_SPC,           _______, _______, _______, _______, _______, _______
 ),
@@ -120,7 +120,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------|------+------+------+------+------+------+------+------+------+------+------|
  * |  `   |  1   |  2   |  3   |  4   |  5   |  6   |  7   |  8   |  9   |  0   |  -   |
  * |------|------+------+------+------+------+------+------+------+------+------+------|
- * |Enter |  {   |  }   |  [   |  ]   |  _   | M-f  |  \   |   ,  |  .   |  /   |  +   |
+ * |      |  {   |  }   |  [   |  ]   |  _   | M-f  |  \   |   ,  |  .   |  /   |  +   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |Adjust|    Space    |xxxxxx|      |      |      |      |
  * `-----------------------------------------------------------------------------------'
@@ -128,7 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_planck_grid(
     KC_TILD, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_EQL ,
     KC_GRV,  KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS,
-    KC_ENT,  KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_UNDS, A(KC_F), _______, _______, _______, _______, KC_PLUS,
+    _______, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_UNDS, A(KC_F), KC_BSLS, _______, _______, _______, KC_PLUS,
     _______, _______, _______, _______, _______, KC_SPC,           _______, _______, _______, _______, _______, _______
 ),
 
@@ -153,19 +153,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Adjust (Lower + Raise)
  * TODO: special chars above fun keys
  * ,-----------------------------------------------------------------------------------.
- * | <Div>|      |      |      |Qwerty|Colemk|Dvorak| Math | Greek|      |      |      |
+ * |      | <F9> | <F10>| <F11>| <F12>|Colemk|Dvorak|      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | <F1> | <F2> | <F3> | <F4> | <F5> | <F6> | <F7> | <F8> | <F9> | <F10>| <F11>| <F12>|
+ * |      | <F5> | <F6> | <F7> | <F8> |Qwerty|      | PvTrk| Play | NxTrk|      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Shift| GUI  | Alt  |      |      |      |      | Alt  | GUI  | Shift| Ctrl |
+ * |      | <F1> | <F2> | <F3> | <F4> |      |      | VolDn| Mute | VolUp|      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |xxxxxx|             |xxxxxx|      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_planck_grid(
- UC(0x00F7), _______, _______, _______, QWERTY , COLEMAK, DVORAK , _______, _______, _______, _______, _______,
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12 ,
-    KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, _______, _______, _______, _______, KC_RALT, KC_LGUI, KC_RSFT, KC_RCTL,
+    _______, KC_F9  , KC_F10 , KC_F11 , KC_F12 , COLEMAK, DVORAK , _______, _______, _______, _______, _______,
+    _______, KC_F5  , KC_F6  , KC_F7  , KC_F8  , QWERTY , _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______,
+    _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , _______, _______, KC_VOLD, KC_MUTE, KC_VOLU, _______, _______,
     _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______
 )
 
